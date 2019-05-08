6e8e0bcc3a1e8d2d10a046d99dcff8a2ecc9b1de
Wim Lemkens <wim@avoid.be>
2019-05-08 10:47:31 +0200

    Reformatted the papirus display class

--- source/Display/PapirusDisplay.py
+++ source/Display/PapirusDisplay.py
@@ -10,26 +10,43 @@ import time
 WHITE = 1
 BLACK = 0
 
+
 class PapirusDisplay(object):
-	def __init__(self, config):
-		self.fontPath = "/usr/local/share/fonts/Righteous-Regular.ttf"
-		self.my_papirus = Papirus()
-		self.draw()
+    def __init__(self, config):
+        self.fontPath = "/usr/local/share/fonts/Righteous-Regular.ttf"
+        self.my_papirus = Papirus()
+        self.setup, self.mqtt = self.loadConfig(config)
+        self.
+        self.draw()
 
-	def draw(self):
-		# initially set all white background
-		image = Image.new('1', self.my_papirus.size, WHITE)
 
-		# prepare for drawing
-		draw = ImageDraw.Draw(image)
-		font = ImageFont.truetype(self.fontPath, 12)
-		printstring = "TEST2"
-		draw.text((10, 40), printstring, font=font, fill=BLACK)
+    def loadConfig(self, config):
+        setup = None
+        mqtt = None
+        with open(filename) as f:
+            config = json.load(f)
+        mqttConfig = config["mqtt"]["configFile"]
+        setupConfig = config["setup"]["configFile"]
+        with open(setupConfig) as f2:
+            setup = Setup(json.load(f2))
+        with open(setupConfig) as f2:
+            mqtt = json.load(f2)
+        return setup, mqtt
 
-		self.my_papirus.display(image)
-		partial = True
-		if partial:
-			self.my_papirus.partial_update()
-		else:
-			self.my_papirus.update()
 
+    def draw(self):
+        # initially set all white background
+        image = Image.new('1', self.my_papirus.size, WHITE)
+
+        # prepare for drawing
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype(self.fontPath, 12)
+        printstring = "TEST2"
+        draw.text((10, 40), printstring, font=font, fill=BLACK)
+
+        self.my_papirus.display(image)
+        partial = True
+        if partial:
+            self.my_papirus.partial_update()
+        else:
+            self.my_papirus.update()
