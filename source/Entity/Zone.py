c3da60485c73a20c6d1c97916b7333d07ffbf1c4
Wim Lemkens <wim.lemkens@gmail.com>
2019-05-08 10:39:57 +0200

    Added zone entity for display

---
+++ source/Entity/Zone.py
@@ -0,0 +1,20 @@
+class Zone(object):
+	def __init__(self, config):
+		self.temperature = None
+		self.setpoint = None
+		self.name = None
+		self.topicTemp = None
+		self.topicSP = None
+		self.icon = None
+
+	def getTemperature():
+		return self.temperature
+
+	def getSetpoint():
+		return self.setpoint
+
+	def getName()
+		return self.name
+
+	def isEnabled():
+		return True
