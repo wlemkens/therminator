253de55d38805399ed523a369f42b5ae77a03ac1
Wim Lemkens <wim@avoid.be>
2019-05-08 11:53:10 +0200

    Untested first attempt to display the data

--- source/Display/PapirusDisplay.py
+++ source/Display/PapirusDisplay.py
@@ -7,6 +7,10 @@ import sys
 import os
 import time
 
+import json
+
+from Entity.Zone import Zone
+
 WHITE = 1
 BLACK = 0
 
@@ -16,9 +20,51 @@ class PapirusDisplay(object):
         self.fontPath = "/usr/local/share/fonts/Righteous-Regular.ttf"
         self.my_papirus = Papirus()
         self.setup, self.mqtt = self.loadConfig(config)
-        self.
-        self.draw()
+        self.createLayout(self.setup, self.mqtt)
+        self.zones = []
 
+    def getFontSize(self, area, printstring):
+        # returns (ideal fontsize, (length of text, height of text)) that maximally
+        # fills a papirus object for a given string
+        fontsize = 0
+        stringlength = 0
+        stringwidth = 0
+
+        maxLength = area[1]
+        maxHeight = area[0]
+
+        while (stringlength <= maxLength and stringwidth <= maxHeight):
+
+            fontsize += 1
+            font = ImageFont.truetype(self.fontPath, fontsize)
+            size = font.getsize(printstring)
+            stringlength = size[0]
+            stringwidth = size[1]
+
+        #font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', fontsize-1)
+        font = ImageFont.truetype(self.fontPath, fontsize-1)
+        return fontsize-1, font.getsize(printstring)
+
+    def updateZone(self, zone, index, draw):
+        lineHeight = 1.0 * self.my_papirus.height / len(self.zones)
+        lineWidth = self.my_papirus.width / 3
+        text = "{:}: {:}/{:}".format(zone.getName(), zone.getTemperature(), zone.getSetpoint())
+        fontSize = self.getFontSize([lineWidth, lineHeight], text)
+        font = ImageFont.truetype(self.fontPath, 12)
+        draw.text((lineWidth * 2, lineHeight * (index + 1)), text, font=font, fill=BLACK)
+
+    def update(self):
+        i = 0
+        image = Image.new('1', self.my_papirus.size, WHITE)
+        draw = ImageDraw.Draw(image)
+        for zone in self.zones:
+            self.updateZone(zone, i, draw)
+            i += 1
+        self.my_papirus.partial_update()
+
+    def createLayout(self, setup, mqtt):
+        for zone in setup["zones"].keys():
+            self.zones += Zone(zone, setup["zones"][zone], mqtt, self.update)
 
     def loadConfig(self, config):
         setup = None
@@ -28,7 +74,7 @@ class PapirusDisplay(object):
         mqttConfig = config["mqtt"]["configFile"]
         setupConfig = config["setup"]["configFile"]
         with open(setupConfig) as f2:
-            setup = Setup(json.load(f2))
+            setup = json.load(f2)
         with open(setupConfig) as f2:
             mqtt = json.load(f2)
         return setup, mqtt
@@ -40,7 +86,6 @@ class PapirusDisplay(object):
 
         # prepare for drawing
         draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype(self.fontPath, 12)
         printstring = "TEST2"
         draw.text((10, 40), printstring, font=font, fill=BLACK)
 
--- source/Entity/Zone.py
+++ source/Entity/Zone.py
@@ -1,11 +1,37 @@
+import paho.mqtt.client as mqtt
+
 class Zone(object):
-	def __init__(self, config):
+	def __init__(self, name, config, mqttConfig, on_update):
 		self.temperature = None
 		self.setpoint = None
-		self.name = None
-		self.topicTemp = None
-		self.topicSP = None
-		self.icon = None
+		self.name = name
+		self.topicTemp = "therminator/in/{:}_temperature".format(name)
+		self.topicSP = "therminator/in/{:}_setpoint".format(name)
+		self.icon = config["icon"]
+		self.connect(mqttConfig)
+		self.on_update = on_update
+
+	def on_message(self, client, userdata, message):
+		if message.topic == self.topicTemp:
+			self.temperature = float(message.payload)
+		elif message.topic == self.topicSp:
+			self.setpoint = float(message.payload)
+
+	def requestValues(self):
+		topic = "therminator/request"
+		requestMessageTemp = "\"{:}_temperature\"".format(self.name)
+		requestMessageSP = "\"{:}_setpoint\"".format(self.name)
+		self.client.publish(topic, requestMessageTemp)
+		self.client.publish(topic, requestMessageSP)
+
+	def connect(self, mqttConfig):
+		self.client = mqtt.Client()
+		self.client.on_message = self.on_message
+		self.client.connect(mqttConfig["address"], mqttConfig["port"], 60)
+		self.client.loop_start()
+		topics = [(self.topicTemp,1),(self.topicSP,1)]
+		r = self.client.subscribe(topics)
+		self.requestValues()
 
 	def getTemperature():
 		return self.temperature
@@ -18,3 +44,6 @@ class Zone(object):
 
 	def isEnabled():
 		return True
+
+	def update(self):
+		self.on_update()
