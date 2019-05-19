4516391b5f6b5e10741fcdf2fd94eab8b81453a9
Wim Lemkens <wim@avoid.be>
2019-05-19 09:03:52 +0200

    error sum limit is now with respect to the I action

--- source/Controller/PID.py
+++ source/Controller/PID.py
@@ -55,7 +55,7 @@ class PID(object):
             if dt > 0:
                 errorDif = (error - self.getEarlierError()) / self.historyRange.seconds
                 self.errorSum += error * dt
-                self.errorSum = max(min(self.errorSum, self.errorSumLimit), -self.errorSumLimit)
+                self.errorSum = max(min(self.errorSum, self.errorSumLimit/self.I), -self.errorSumLimit/self.I)
             self.lastError = error
             print(self.P * error, self.I * self.errorSum, self.D * errorDif)
             result = self.P * error + self.I * self.errorSum + self.D * errorDif
