3e16b64c21453e3eed23776c0414df95b1e193b7
Wim Lemkens <wim@avoid.be>
2019-06-15 15:11:02 +0200

    Storing of setpoint

--- source/Entity/Zone.py
+++ source/Entity/Zone.py
@@ -1,71 +1,98 @@
 import paho.mqtt.client as mqtt
 
+import time
+
 class Zone(object):
-	def __init__(self, config, mqttConfig, on_update):
-		self.temperature = None
-		self.setpoint = None
-		self.level = None
-		self.enabled = True
-		self.name = config["id"]
-		self.label = config["label"]
-		self.topicTemp = "therminator/in/{:}_temperature".format(self.name)
-		self.topicSP = "therminator/in/{:}_setpoint".format(self.name)
-		self.topicLvl = "therminator/in/{:}_level".format(self.name)
-		self.topicEnabled = "therminator/in/{:}_enabled".format(self.name)
-		self.icon = config["icon"]
-		self.connect(mqttConfig)
-		self.on_update = on_update
+    def __init__(self, config, mqttConfig, on_update):
+        self.setpointOFF = 15.0
+        self.temperature = None
+        self.setpoint = None
+        self.stored_setpoint = self.setpointOFF
+        self.level = None
+        self.enabled = True
+        self.name = config["id"]
+        self.label = config["label"]
+        self.topicTemp = "therminator/in/{:}_temperature".format(self.name)
+        self.topicSP = "therminator/in/{:}_setpoint".format(self.name)
+        self.topicLvl = "therminator/in/{:}_level".format(self.name)
+        self.topicEnabled = "therminator/in/{:}_enabled".format(self.name)
+        self.icon = config["icon"]
+        self.connect(mqttConfig)
+        self.on_update = on_update
 
-	def on_message(self, client, userdata, message):
-		if message.topic == self.topicTemp:
-			self.temperature = float(message.payload)
-		elif message.topic == self.topicSP:
-			self.setpoint = float(message.payload)
-		elif message.topic == self.topicLvl:
-			self.level = float(message.payload)
-		elif message.topic == self.topicEnabled:
-			self.enabled = float(message.payload)
-		self.update()
+    def setSetpoint(self, setpoint):
+        topic = "therminator/out/{:}_setpoint".format(self.name)
+        self.client.publish(topic, setpoint)
 
-	def requestValues(self):
-		topic = "therminator/request"
-		requestMessageTemp = "\"{:}_temperature\"".format(self.name)
-		requestMessageSP = "\"{:}_setpoint\"".format(self.name)
-		requestMessageLvl = "\"{:}_level\"".format(self.name)
-		requestMessageEnabled = "\"{:}_enabled\"".format(self.name)
-		self.client.publish(topic, requestMessageTemp)
-		self.client.publish(topic, requestMessageSP)
-		self.client.publish(topic, requestMessageLvl)
-		self.client.publish(topic, requestMessageEnabled)
+    def storeSetpoint(self, setpoint):
+        topic = "therminator/out/{:}_stored_setpoint".format(self.name)
+        self.stored_setpoint = self.setpoint
+        self.client.publish(topic, setpoint)
 
-	def connect(self, mqttConfig):
-		self.client = mqtt.Client()
-		self.client.on_message = self.on_message
-		self.client.connect(mqttConfig["address"], mqttConfig["port"], 60)
-		self.client.loop_start()
-		topics = [(self.topicTemp,1),(self.topicSP,1),(self.topicLvl,1),(self.topicEnabled,1)]
-		self.client.loop_start()
-		r = self.client.subscribe(topics)
-		self.requestValues()
-                
+    def setStatus(self, status):
+        if status:
+            self.setSetpoint(self.stored_setpoint)
+        else:
+            self.storingSetpoint = True
+            self.storeSetpoint(self.setpoint)
+            self.setSetpoint(self.setpointOFF)
+            time.sleep(5)
+            self.storingSetpoint = False
 
-	def getTemperature(self):
-		return self.temperature
+    def on_message(self, client, userdata, message):
+        if message.topic == self.topicTemp:
+            self.temperature = float(message.payload)
+        elif message.topic == self.topicSP:
+            if self.enabled:
+                self.setpoint = float(message.payload)
+            else:
+                if not self.storingSetpoint:
+                    self.storeSetpoint(float(message.payload))
+        elif message.topic == self.topicLvl:
+            self.level = float(message.payload)
+        elif message.topic == self.topicEnabled:
+            self.enabled = float(message.payload)
+            self.setStatus(self.enabled)
+        self.update()
 
-	def getSetpoint(self):
-		return self.setpoint
+    def requestValues(self):
+        topic = "therminator/request"
+        requestMessageTemp = "\"{:}_temperature\"".format(self.name)
+        requestMessageSP = "\"{:}_setpoint\"".format(self.name)
+        requestMessageLvl = "\"{:}_level\"".format(self.name)
+        requestMessageEnabled = "\"{:}_enabled\"".format(self.name)
+        self.client.publish(topic, requestMessageTemp)
+        self.client.publish(topic, requestMessageSP)
+        self.client.publish(topic, requestMessageLvl)
+        self.client.publish(topic, requestMessageEnabled)
 
-	def getName(self):
-		return self.name
+    def connect(self, mqttConfig):
+        self.client = mqtt.Client()
+        self.client.on_message = self.on_message
+        self.client.connect(mqttConfig["address"], mqttConfig["port"], 60)
+        self.client.loop_start()
+        topics = [(self.topicTemp, 1), (self.topicSP, 1), (self.topicLvl, 1), (self.topicEnabled, 1)]
+        self.client.loop_start()
+        r = self.client.subscribe(topics)
+        self.requestValues()
 
-	def getLabel(self):
-		return self.label
+    def getTemperature(self):
+        return self.temperature
 
-	def isEnabled(self):
-		return self.enabled
+    def getSetpoint(self):
+        return self.setpoint
 
-	def update(self):
-		self.on_update()
+    def getName(self):
+        return self.name
 
-	def getLevel(self):
-		return self.level
+    def getLabel(self):
+        return self.label
+
+    def isEnabled(self):
+        return self.enabled
+
+    def update(self):
+        self.on_update()
+
+    def getLevel(self):
+        return self.level
