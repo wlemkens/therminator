a40b544f1a560f4694f34b365001fd3bb5fafd80
Wim Lemkens <wim.lemkensmail.com>
2020-12-23 20:06:12 +0100

    Thermostat is now waiting for dependencies for initialization

--- source/Scheduler/ScheduleSwitcher.py
+++ source/Scheduler/ScheduleSwitcher.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from Scheduler.SchedulerFactory import SchedulerFactory
 
@@ -10,29 +11,41 @@ from MQTT.MqttProvider import MqttProvider
 class ScheduleSwitcher(object):
     def __init__(self, config):
         self.topicSchedule = "therminator/in/schedule"
-        schedulerType = config["scheduler"]["type"]
-        controllerTypes = config["controllers"]
-        setupFile = config["setup"]["configFile"]
+        self.schedulerType = config["scheduler"]["type"]
+        self.controllerTypes = config["controllers"]
+        self.setupFile = config["setup"]["configFile"]
         schedulerConfigFilename = config["scheduler"]["configFile"]
         self.scheduleName = config["scheduler"]["default"]
-        boilerType = config["boiler_controller"]["type"]
-        boilerConfig = config["boiler_controller"]["configFile"]
-        mqttFile = config["mqtt"]["configFile"]
-        with open(mqttFile) as f:
+        self.boilerType = config["boiler_controller"]["type"]
+        self.boilerConfig = config["boiler_controller"]["configFile"]
+        self.mqttFile = config["mqtt"]["configFile"]
+        with open(self.mqttFile) as f:
             mqttConfig = json.load(f)
-        schedulerFactory = SchedulerFactory()
 
         with open(schedulerConfigFilename) as f:
             schedulerConfig = json.load(f)
 
         self.schedules = schedulerConfig["schedules"]
-        schedule = self.schedules[self.scheduleName]
+        self.schedule = self.schedules[self.scheduleName]
         self.modes = schedulerConfig["modes"]
         self.daytypes = schedulerConfig["daytypes"]
-        self.scheduler = schedulerFactory.setupScheduler(schedulerType, controllerTypes, setupFile, schedule, self.modes, self.daytypes, boilerType, boilerConfig,mqttFile)
         self.connect(mqttConfig)
+        self.firstContact = True
         self.watchdog = Watchdog(Modules.THERMOSTAT, [Modules.CONNECTOR, Modules.MONITOR], mqttConfig)
-        self.scheduler.start()
+        self.watchdog.onDependenciesComplete = self.onDependenciesComplete
+        while True:
+            time.sleep(1000)
+
+    def onDependenciesComplete(self):
+        if self.firstContact:
+            print("Loading scheduler")
+            self.firstContact = False
+            schedulerFactory = SchedulerFactory()
+            self.scheduler = schedulerFactory.setupScheduler(self.schedulerType, self.controllerTypes, self.setupFile, self.schedule, self.modes, self.daytypes, self.boilerType, self.boilerConfig, self.mqttFile)
+            topics = [(self.topicSchedule, 1)]
+            r = self.client.subscribe(self, topics)
+            self.client.publish("therminator/request","schedule")
+            self.scheduler.start()
 
     def selectSchedule(self, scheduleName):
         self.scheduler.loadConfig(self.schedules[scheduleName], self.modes, self.daytypes)
@@ -40,9 +53,6 @@ class ScheduleSwitcher(object):
     def connect(self, mqttConfig):
         self.client = MqttProvider(mqttConfig["address"], mqttConfig["port"])
 #        self.client.loop_start()
-        topics = [(self.topicSchedule, 1)]
-        r = self.client.subscribe(self, topics)
-        self.client.publish("therminator/request","schedule")
 
     def on_message(self, client, userdata, message):
         if message.topic == self.topicSchedule:
