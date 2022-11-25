e2b8307f2e482cd9c8bbd9671b134a4924fa967b
Wim Lemkens <wim.lemkens@gmail.com>
2022-11-25 10:36:40 +0100

    Set the logging default to info

--- source/thermostat.py
+++ source/thermostat.py
@@ -6,13 +6,18 @@ from Scheduler.ScheduleSwitcher import ScheduleSwitcher
 # General imports
 import sys
 import json
+import logging
+import datetime
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        log_level = logging.INFO;
+        logging.basicConfig(filename='/var/log/thermostat.log', level=log_level)
+        logging.info(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Starting thermostat")
         filename = sys.argv[1]
         config = {}
         with open(filename) as f:
             config = json.load(f)
-        scheduleSwitcher = ScheduleSwitcher(config)
+        scheduleSwitcher = ScheduleSwitcher(config, log_level)
     else:
         print("Usage {:} </path/to/config.json>".format(sys.argv[0]))
